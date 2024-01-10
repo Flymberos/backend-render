ef59da4ab57c64554c1ef5e62803279f46d68d1f
Benjamin Pasic <benjamin.pasic1@gmail.com>
2024-01-10 20:54:01 +0100

    fix

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require("express");
 const morgan = require("morgan");
 const app = express();
 const cors = require("cors");
-const port = process.env.port || 3001;
+const port = process.env.PORT || 3001;
 
 const unknownEndpoint = (request, response) => {
   response.status(404).send({ error: "unknown endpoint" });
